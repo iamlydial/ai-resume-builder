39640e6fe8bc39f1f77b7e3e5c0f09c5997b8421
iamlydial <lydia.lavecchia@gmail.com>
2024-10-05 21:35:27 +0200

    Update index.jsx
    add the button to manage the user

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/clerk-react";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return <div>
+    <UserButton/>
+  </div>;
 };
 
 export default Home;
