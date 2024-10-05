108b128fd0404083b6299147e44d4587f633a72a
iamlydial <lydia.lavecchia@gmail.com>
2024-10-05 17:10:01 +0200

    add app outlet and children

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Button } from "./components/ui/button";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>
-  <Button>Test Button
-    </Button></>;
+  return (
+    <>
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
