ea634c41cc0031aad6f2ae747149fb1b72c76855
iamlydial <lydia.lavecchia@gmail.com>
2024-10-05 21:30:48 +0200

    add clerk sign in component to sign in

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Button } from "./components/ui/button";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useUser } from "@clerk/clerk-react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const { user, isLoaded, isSignedIn } = useUser();
+
+  if (!isSignedIn&&isLoaded) {
+    return <Navigate to={"/auth/sign-in"} />;
+  }
 
   return (
     <>
--- src/auth/sign-in/index.jsx
+++ src/auth/sign-in/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { SignIn } from "@clerk/clerk-react";
 
 const SignInPage = () => {
-  return <div>Sign In</div>;
+  return (
+    <div className="flex justify-center my-10 items-center">
+      <SignIn />
+    </div>
+  );
 };
 
 export default SignInPage;
