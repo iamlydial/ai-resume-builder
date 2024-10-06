df8dc046fd04e7d6f7ed8f3202277854abc30f73
iamlydial <lydia.lavecchia@gmail.com>
2024-10-06 21:30:05 +0200

    add globalAPI get request for resume and add frontend request to get resume list

--- client/service/GlobalApi.js
+++ client/service/GlobalApi.js
@@ -2,13 +2,16 @@ import axios from "axios";
 const API_KEY = import.meta.env.VITE_STRAPI_API_KEY;
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:1337/api/user-resumes",
+  baseURL: "http://localhost:1337/api/",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${API_KEY}`,
   },
 });
 
-const CreateNewResume = (data) => axiosClient.post('',data);
+const CreateNewResume = (data) => axiosClient.post("/user-resumes", data);
 
-export default { CreateNewResume };
+const GetUserResumes = (userEmail) =>
+  axiosClient.get("/user-resumes?filters[userEmail][$eq]=" + userEmail);
+
+export default { CreateNewResume, GetUserResumes };
--- client/src/dashboard/index.jsx
+++ client/src/dashboard/index.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AddResume from "./components/AddResume";
+import { useUser } from "@clerk/clerk-react";
+import GlobalApi from "../../service/GlobalApi.js";
 
 const Dashboard = () => {
+  const { user } = useUser();
+  const [resumeList, setResumeList] = useState();
+
+  useEffect(() => {
+    user && GetResumesList();
+  }, [user]);
+
+  const GetResumesList = () => {
+    GlobalApi.GetUserResumes(user?.primaryEmailAddress?.emailAddress).then(
+      (resp) => {
+        setResumeList(resp.data);
+      }
+    );
+  };
+
   return (
     <div className="p-10 mx: px-20">
       <h2 className="font-bold text-3xl">My Resume</h2>
       <p>Start creating AI resume for your next role. </p>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mt-10">
         <AddResume />
+        {resumeList.map((resume, index)=>(''))}
       </div>
     </div>
   );
