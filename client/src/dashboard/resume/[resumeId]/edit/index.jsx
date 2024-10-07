1c3097d595280d8f872fb88d8af685199eb5467a
iamlydial <lydia.lavecchia@gmail.com>
2024-10-07 14:31:07 +0200

    add resume sections

--- client/src/dashboard/resume/[resumeId]/edit/index.jsx
+++ client/src/dashboard/resume/[resumeId]/edit/index.jsx
@@ -1,15 +1,23 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import FormSection from "../../components/FormSection";
+import ResumeReview from "../../components/ResumeReview";
 
 const EditResume = () => {
-    const params = useParams();
-    
-    useEffect(()=>{
-        console.log(params, "params")
-    },[])
+  const params = useParams();
 
+  useEffect(() => {
+    console.log(params, "params");
+  }, []);
 
-  return <div>EditResume</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 p-10 gap-10">
+      {/* Form Section */}
+      <FormSection />
+      {/* Preview Section */}
+      <ResumeReview />
+    </div>
+  );
 };
 
 export default EditResume;
---
+++ client/src/dashboard/resume/components/ResumeReview.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ResumeReview = () => {
+  return <div>ResumeReview</div>;
+};
+
+export default ResumeReview;
