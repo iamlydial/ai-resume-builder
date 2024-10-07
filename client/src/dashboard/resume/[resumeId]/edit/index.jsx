01b113b7c77ef2db5f1712c2f2764a558a97d265
iamlydial <lydia.lavecchia@gmail.com>
2024-10-07 15:24:29 +0200

    adds summary section of the resume

--- client/src/dashboard/resume/[resumeId]/edit/index.jsx
+++ client/src/dashboard/resume/[resumeId]/edit/index.jsx
@@ -1,22 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import FormSection from "../../components/FormSection";
 import ResumeReview from "../../components/ResumeReview";
+import { ResumeInfoContext } from "@/context/ResumeInfoContext";
+import dummy from "@/data/dummy";
 
 const EditResume = () => {
   const params = useParams();
+  const [resumeInfo, setResumeInfo] = useState();
 
   useEffect(() => {
-    console.log(params, "params");
+    setResumeInfo(dummy);
   }, []);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 p-10 gap-10">
-      {/* Form Section */}
-      <FormSection />
-      {/* Preview Section */}
-      <ResumeReview />
-    </div>
+    <ResumeInfoContext.Provider value={{ resumeInfo, setResumeInfo }}>
+      <div className="grid grid-cols-1 md:grid-cols-2 p-10 gap-10">
+        {/* Form Section */}
+        <FormSection />
+        {/* Preview Section */}
+        <ResumeReview />
+      </div>
+    </ResumeInfoContext.Provider>
   );
 };
 
--- client/src/dashboard/resume/components/ResumeReview.jsx
+++ client/src/dashboard/resume/components/ResumeReview.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import { ResumeInfoContext } from "@/context/ResumeInfoContext";
+import React, { useContext } from "react";
+import PersonalDetailPreview from "./preview/PersonalDetailPreview";
 
 const ResumeReview = () => {
+  const { resumeInfo, setResumeInfo } = useContext(ResumeInfoContext);
+
   return (
-    <div>
+    <div
+      className="shadow-lg h-full p-14 border-t-[20px]"
+      style={{ borderColor: resumeInfo?.themeColor }}
+    >
       {/* Personal Details */}
+      <PersonalDetailPreview resumeInfo={resumeInfo} />
       {/* Summary */}
       {/* Professional Experience */}
       {/* Education */}
