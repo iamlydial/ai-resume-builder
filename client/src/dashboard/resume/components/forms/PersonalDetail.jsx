a61437c5a340a6415ad1e3fc67ed307f08dc95a3
iamlydial <lydia.lavecchia@gmail.com>
2024-10-08 09:27:03 +0200

    Update PersonalDetail.jsx

--- client/src/dashboard/resume/components/forms/PersonalDetail.jsx
+++ client/src/dashboard/resume/components/forms/PersonalDetail.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { ResumeInfoContext } from "@/context/ResumeInfoContext";
+import React, { useContext } from "react";
 
 const PersonalDetail = () => {
-  return <div>PersonalDetail</div>;
+  const { resumeInfo, setResumeInfo } = useContext(ResumeInfoContext);
+  return (
+    <div className="p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10">
+      <h2 className="font-bold text-lg">
+        Personal Detail
+      </h2>
+      <p>get started with basic information</p>
+    </div>
+  );
 };
 
 export default PersonalDetail;
