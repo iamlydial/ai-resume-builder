4fdd9a73200c374139203b18b346c94d59d14428
iamlydial <lydia.lavecchia@gmail.com>
2024-10-08 11:13:09 +0200

    Update PersonalDetail.jsx
    Added default value for inputs field in PersonalDetail component to populate with resumeInfo data

--- client/src/dashboard/resume/components/forms/PersonalDetail.jsx
+++ client/src/dashboard/resume/components/forms/PersonalDetail.jsx
@@ -54,27 +54,27 @@ const PersonalDetail = ({ enableText }) => {
         <div className="grid grid-cols-2 mt-5 gap-3">
           <div>
             <label className="text-sm">First Name</label>
-            <Input name="firstName" required onChange={handleInputChange} />
+            <Input name="firstName" defaultValue={resumeInfo?.firstName} required onChange={handleInputChange} />
           </div>
           <div>
             <label className="text-sm">Last Name</label>
-            <Input name="lastName" required onChange={handleInputChange} />
+            <Input name="lastName" defaultValue={resumeInfo?.lastName} required onChange={handleInputChange} />
           </div>
           <div className="col-span-2">
             <label className="text-sm">Job Title</label>
-            <Input name="jobTitle" required onChange={handleInputChange} />
+            <Input name="jobTitle" defaultValue={resumeInfo?.jobTitle} required onChange={handleInputChange} />
           </div>
           <div className="col-span-2">
             <label className="text-sm">Address</label>
-            <Input name="address" required onChange={handleInputChange} />
+            <Input name="address" defaultValue={resumeInfo?.address} required onChange={handleInputChange} />
           </div>
           <div className="text-sm">
             <label className="text-sm">Phone</label>
-            <Input name="phone" required onChange={handleInputChange} />
+            <Input name="phone" defaultValue={resumeInfo?.phone} required onChange={handleInputChange} />
           </div>
           <div className="text-sm">
             <label className="text-sm">Email</label>
-            <Input name="email" required onChange={handleInputChange} />
+            <Input name="email" defaultValue={resumeInfo?.email} required onChange={handleInputChange} />
           </div>
         </div>
         <div className="mt-3 flex justify-end">
