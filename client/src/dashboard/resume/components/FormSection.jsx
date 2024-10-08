08370b3ffe26138b562fd2f12a6bb11df26d08f5
iamlydial <lydia.lavecchia@gmail.com>
2024-10-08 09:43:09 +0200

    Update FormSection.jsx
    adds UI and activeForm index to go through the different form sections

--- client/src/dashboard/resume/components/FormSection.jsx
+++ client/src/dashboard/resume/components/FormSection.jsx
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import PersonalDetail from "./forms/PersonalDetail";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft, ArrowRight, LayoutGrid } from "lucide-react";
 
 const FormSection = () => {
-  return <div>
-    {/* Personal Detail Form */}
-    <PersonalDetail/>
-    {/* Summery */}
-    {/* Experience */}
-    {/* Educational Detail */}
-    {/* Skills */}
-  </div>;
+  const [activeFormIndex, setActiveFormIndex] = useState(1);
+  return (
+    <div>
+      <div className="flex justify-between items-center">
+        <Button variant="outline" size="sm" className="flex gap-2">
+          <LayoutGrid />
+          Theme
+        </Button>
+
+        <div className="flex gap-2">
+          {activeFormIndex > 1 && (
+            <Button
+              className="flex gap-2"
+              size="sm"
+              onClick={() => setActiveFormIndex(activeFormIndex - 1)}
+            >
+              <ArrowLeft />
+            </Button>
+          )}
+          <Button
+            className="flex gap-2"
+            size="sm"
+            onClick={() => setActiveFormIndex(activeFormIndex + 1)}
+          >
+            Next
+            <ArrowRight />
+          </Button>
+        </div>
+      </div>
+      {/* Personal Detail Form */}
+      {activeFormIndex == 1 ? <PersonalDetail /> : null}
+
+      {/* Summery */}
+      {/* Experience */}
+      {/* Educational Detail */}
+      {/* Skills */}
+    </div>
+  );
 };
 
 export default FormSection;
