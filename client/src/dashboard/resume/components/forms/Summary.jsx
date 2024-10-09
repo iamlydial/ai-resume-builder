541f92f8db07fadca66caa9721c56cae220e9b42
iamlydial <lydia.lavecchia@gmail.com>
2024-10-09 11:35:57 +0200

    Update Summary.jsx

--- client/src/dashboard/resume/components/forms/Summary.jsx
+++ client/src/dashboard/resume/components/forms/Summary.jsx
@@ -1,10 +1,25 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Summary = () => {
   return (
-    <div className="p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10">
-      <h2 className="font-bold text-lg">Summary</h2>
-      <p>Add Summary for your Job Title</p>
+    <div>
+      <div className="p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10">
+        <h2 className="font-bold text-lg">Summary</h2>
+        <p>Add Summary for your Job Title</p>
+        <div className="mt-7">
+          <div className="flex justify-between items-end">
+            <label>Add Summary</label>
+            <Button
+              variant="outline"
+              className="border-primary text-primary"
+              size="sm"
+            >
+              Generate From AI
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
