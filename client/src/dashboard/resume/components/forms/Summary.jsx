f2bdc5d51db9ae183ce2963a548603c57e1b2ea0
iamlydial <lydia.lavecchia@gmail.com>
2024-10-09 11:30:58 +0200

    Update Summary.jsx

--- client/src/dashboard/resume/components/forms/Summary.jsx
+++ client/src/dashboard/resume/components/forms/Summary.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Summary = () => {
   return (
-    <div>
-      Summary
+    <div className="p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10">
+      <h2 className="font-bold text-lg">Summary</h2>
+      <p>Add Summary for your Job Title</p>
     </div>
-  )
-}
+  );
+};
 
-export default Summary
+export default Summary;
