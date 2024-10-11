64dd6b2a2d0b4ea42d3bf311a6f169efc447fcc8
iamlydial <lydia.lavecchia@gmail.com>
2024-10-11 09:40:52 +0200

    Update Education.jsx
    Added `educationField` constant to initialize the structure for education details, including university name, degree, major, start date, end date, and description.

--- client/src/dashboard/resume/components/forms/Education.jsx
+++ client/src/dashboard/resume/components/forms/Education.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+
+const educationField = {
+  universityName: "",
+  degree: "",
+  major: "",
+  startDate: "",
+  endDate: "",
+  description: "",
+};
 
 const Education = ({ enabledNext }) => {
+  const [educationalList, setEducationalList] = useState([educationField]);
+
   return (
     <div className="p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10">
       <h2 className="font-bold text-lg">Education</h2>
