8b8f9124ca99ea186877f5e1b2eb179bb9791189
iamlydial <lydia.lavecchia@gmail.com>
2024-10-16 14:25:52 +0200

    add render backend deployed url

--- ai-builder-admin/types/generated/components.d.ts
+++ ai-builder-admin/types/generated/components.d.ts
@@ -1,14 +1,19 @@
 import type { Struct, Schema } from '@strapi/strapi';
 
-export interface SkillsSkills extends Struct.ComponentSchema {
-  collectionName: 'components_skills_skills';
+export interface EducationEducation extends Struct.ComponentSchema {
+  collectionName: 'components_education_educations';
   info: {
-    displayName: 'skills';
-    icon: 'puzzle';
+    displayName: 'Education';
+    icon: 'book';
+    description: '';
   };
   attributes: {
-    name: Schema.Attribute.String;
-    rating: Schema.Attribute.Integer;
+    universityName: Schema.Attribute.String;
+    degree: Schema.Attribute.String;
+    major: Schema.Attribute.String;
+    startDate: Schema.Attribute.String;
+    endDate: Schema.Attribute.String;
+    description: Schema.Attribute.Text;
   };
 }
 
@@ -29,29 +34,24 @@ export interface ExperienceExperience extends Struct.ComponentSchema {
   };
 }
 
-export interface EducationEducation extends Struct.ComponentSchema {
-  collectionName: 'components_education_educations';
+export interface SkillsSkills extends Struct.ComponentSchema {
+  collectionName: 'components_skills_skills';
   info: {
-    displayName: 'Education';
-    icon: 'book';
-    description: '';
+    displayName: 'skills';
+    icon: 'puzzle';
   };
   attributes: {
-    universityName: Schema.Attribute.String;
-    degree: Schema.Attribute.String;
-    major: Schema.Attribute.String;
-    startDate: Schema.Attribute.String;
-    endDate: Schema.Attribute.String;
-    description: Schema.Attribute.Text;
+    name: Schema.Attribute.String;
+    rating: Schema.Attribute.Integer;
   };
 }
 
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
-      'skills.skills': SkillsSkills;
-      'experience.experience': ExperienceExperience;
       'education.education': EducationEducation;
+      'experience.experience': ExperienceExperience;
+      'skills.skills': SkillsSkills;
     }
   }
 }
--- client/service/GlobalApi.js
+++ client/service/GlobalApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const API_KEY = import.meta.env.VITE_STRAPI_API_KEY;
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:1337/api/",
+  baseURL: import.meta.env.VITE_BASE_URL + "api/",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${API_KEY}`,
@@ -24,7 +24,6 @@ const GetResumeById = (id) => {
 const DeleteResumeById = (id) => {
   return axiosClient.delete("/user-resumes/" + id);
 };
-
 
 export default {
   CreateNewResume,
